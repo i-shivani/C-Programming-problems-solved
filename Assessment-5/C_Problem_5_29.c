09935663b1cd7e2754c449c15a86d3a40bc3b284
i-shivani <i.shivani.2807@gmail.com>
2025-12-14 23:18:10 +0530

    Add files via upload

---
+++ Assessment-5/C_Problem_5_29.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z,n,a,b=0,lcm=1,p,hcf;
+    printf("Enter number1: ");
+    scanf("%d",&x);
+    printf("Enter number2: ");
+    scanf("%d",&y);
+    printf("Enter number3: ");
+    scanf("%d",&z);
+    p=x*y*z;
+    if(x>y)
+        n=x;
+    if(y>x)
+        n=y;
+    p=x*y;
+    for(a=n/2;a > 0;a--)
+    {
+        if(x%a == 0 && y%a == 0 && b == 0)
+        {
+            lcm=p/a;
+            b+=1;
+        }
+        if(a==1 && y!=z)
+        {
+            x=lcm;
+            y=z;
+            p=x*y;
+            if(x>y)
+                n=x;
+            if(y>x)
+                n=y;
+            b=0;
+            a=(n/2)+1;
+        }
+    }
+    printf("%d\n",lcm);
+}
